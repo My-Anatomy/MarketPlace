8abcb40f99c739676c4969324af1740fa9ac7235
Deepak <deepakviji5556@gmail.com>
2025-09-17 19:06:29 +0530

    Firebase

--- backend/middleware/socketMiddleware.js
+++ backend/middleware/socketMiddleware.js
@@ -1,5 +1,5 @@
 export const socketAuth = (socket, next) => {
-  // Normally verify JWT here, for now just attach dummy user
+
   socket.userId = socket.handshake.auth.userId || 'guest';
   next();
 };
